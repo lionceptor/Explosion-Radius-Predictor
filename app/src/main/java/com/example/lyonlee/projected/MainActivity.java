cd7193b54139c22ccde03703977a618bdbdf3317
Lyon Lee <lyonyjlee@gmail.com>
2018-03-03 12:22:55 -0600

    added view and Input classes
    also added button to maps activity input

--- app/src/main/java/com/example/lyonlee/projected/MainActivity.java
+++ app/src/main/java/com/example/lyonlee/projected/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lyonlee.projected;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void addressRelay(View view)
+    {
+        Intent firstIntent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(firstIntent);
     }
 }
