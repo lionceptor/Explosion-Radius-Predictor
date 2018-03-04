252d966065520c4da75a3d889dd681bf58257e71
Lyon Lee <lyonyjlee@gmail.com>
2018-03-03 18:49:31 -0600

    complete 180 part 2
    geocoder and map moving methods now fully implemented

--- app/src/main/java/com/example/lyonlee/projected/MapsActivity.java
+++ app/src/main/java/com/example/lyonlee/projected/MapsActivity.java
@@ -1,15 +1,24 @@
 package com.example.lyonlee.projected;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -36,21 +45,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    LatLng coordinates;
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-       LatLng grapevine = new LatLng(32.155352, -97.119709);
-        mMap.addMarker(new MarkerOptions().position(grapevine).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(grapevine));
-
-      /*  Intent mapsIntent = getIntent();
-        LatLng coordinates1 = mapsIntent.getParcelableExtra("long");
-
-        if(mapsIntent !=null && coordinates1 != null) {
-            LatLng ll = new LatLng(coordinates1.latitude, coordinates1.longitude);
-            mMap.addMarker(new MarkerOptions().position(ll).title("Address"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(ll)); */
-        }
+        coordinates = new LatLng(32.9155352, -97.1197092);
+        mMap.addMarker(new MarkerOptions().position(coordinates).title("Grapevine High School"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        mMap.setMinZoomPreference(14.0f);
     }
+
+    List<Address> addressList = null;
+    String name;
+
+    public void clickSearch(View view)
+    {
+        //get text input from edittext
+        EditText address = (EditText) findViewById(R.id.editText);
+        name = address.getText().toString();
+        //convert text input to coordinates
+        Geocoder converter = new Geocoder(getBaseContext());
+        try {
+            //check if addresssCollection is blank and return null if it is
+            addressList = converter.getFromLocationName(name, 5);
+            if (addressList != null && addressList.size() > 0)
+            {
+                Address xycoord = addressList.get(0);
+                coordinates = new LatLng(xycoord.getLatitude(), xycoord.getLongitude());
+            }
+        }
+        //could throw exception
+        catch(
+                IOException exception)
+        {
+            exception.printStackTrace();
+        }
+        //change map as needed
+        mMap.clear();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(coordinates));
+        mMap.addMarker(new MarkerOptions().position(coordinates).title(name));
+    }
+
+    public void drawNuke(View view)
+    {
+        CircleOptions options = new CircleOptions();
+        options.center(coordinates);
+        options.radius(2000);
+        options.strokeColor(Color.RED);
+        options.fillColor(Color.BLUE);
+        options.strokeWidth(2);
+        mMap.addCircle(options);
+    }
+}
